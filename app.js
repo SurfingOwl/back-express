694cbab46b292ff3495f276e0cd1cd5ef6ca94c1
SurfingOwl <arthur.cesar.fr@gmail.com>
2023-02-28 10:59:21 +0100

    .

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(router)
 
 // Start the app
-const port = process.env.PORT || 3030
+const port = process.env.PORT || 3000
 app.listen(port, function () {
   console.log('listening on port', port)
 })
